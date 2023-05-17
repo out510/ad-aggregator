44db5ad5b886252b0a8819044a0e35bde456b876
out510 <nikita.burak.dev@gmail.com>
2023-05-17 11:05:26 +0300

    rename project

---
+++ rec-sys/src/main/java/bsuir/web/adaggregator/AdAggregatorApplication.java
@@ -0,0 +1,13 @@
+package bsuir.web.adaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdAggregatorApplication.class, args);
+    }
+
+}
