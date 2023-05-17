fa35be79d4f4c91e938054d943808bf1e6f4162a
out510 <nikita.burak.dev@gmail.com>
2023-05-17 11:05:01 +0300

    initialize Spring

---
+++ rec-sys/src/main/java/bsuir/web/recsys/RecSysApplication.java
@@ -0,0 +1,15 @@
+package bsuir.web.recsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecSysApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RecSysApplication.class, args);
+    }
+
+}
