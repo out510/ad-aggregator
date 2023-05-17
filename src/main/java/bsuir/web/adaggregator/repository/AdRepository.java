88146c03a06ca3824707a17c09f19a243ae42335
out510 <nikita.burak.dev@gmail.com>
2023-05-17 11:05:28 +0300

    use AdDefault in AdRepository

--- src/main/java/bsuir/web/adaggregator/repository/AdRepository.java
+++ src/main/java/bsuir/web/adaggregator/repository/AdRepository.java
@@ -1,10 +1,11 @@
 package bsuir.web.adaggregator.repository;
 
-import bsuir.web.adaggregator.domain.Ad;
+import bsuir.web.adaggregator.domain.AdDefault;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
+public interface AdRepository extends PagingAndSortingRepository<AdDefault, Long>, JpaRepository<AdDefault, Long> {
 
 }
