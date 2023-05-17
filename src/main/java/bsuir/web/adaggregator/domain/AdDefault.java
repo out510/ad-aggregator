6dcbbcff945cbf7729ce03259cdcc3d2dc32cccb
out510 <nikita.burak.dev@gmail.com>
2023-05-17 11:05:27 +0300

    add Ad entity

--- src/main/java/bsuir/web/adaggregator/domain/AdDefault.java
+++ src/main/java/bsuir/web/adaggregator/domain/AdDefault.java
@@ -1,9 +1,13 @@
 package bsuir.web.adaggregator.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -11,13 +15,17 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class AdDefault implements Ad {
+    @Id
     private Long id;
     private String title;
     private String description;
     private Double price;
     private String originalUrl;
+    @CreatedDate
     private LocalDateTime createdDate;
+    @LastModifiedDate
     private LocalDateTime updatedDate;
 
     @Override
